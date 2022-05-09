bede3e9e0aaa156eaa67796b9867b97ed269a14b
nosidbar <nosidemean@gmail.com>
2022-05-10 02:06:25 +0900

    useTabs hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+
 import {useInput} from './useInput.js'
+import {useTabs} from './useTabs.js'
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  },
+];
+
 
 const App = () => {
+
+  const {currentItem, changeItem} = useTabs(0, content)
+
   // const maxLen = value => value.length <= 10;
-  const noAt = value => !value.includes('@');
+  const noAt = value => !value.includes('@', content);
   const name = useInput('Mr.', noAt);
+
+  
+
   return  (
     <div className="App">
       <h1>Hello</h1>
       <input placeholder="Name" {...name} />
+      <div className='tab-wrap'>
+        {content.map((section, index) => (
+          <button onClick={()=>changeItem(index)}>{section.tab}</button>
+        ))}
+       <div> {currentItem.content}</div>
+      </div>
+      
     </div>
   );
 
